e8f8a3f0ae6b020dfcedf3075ea4ff517f99df86
Patrick Jimison <pat.jimison@gmail.com>
2026-08-13 10:51:57 -0700

    read and preprocess stopwords.txt before passing it into search_title

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,9 @@
 import argparse
 import json
+import string
 
 from pathlib import Path
-from pkg.search_title import search_title
+from pkg.search_utils import search_title
 
 
 def main() -> None:
@@ -17,15 +18,25 @@ def main() -> None:
     args = parser.parse_args()
 
     movies_path = Path(__file__).parent.parent / "data" / "movies.json"
+    stopwords_path = Path(__file__).parent.parent / "data" / "stopwords.txt"
 
     match args.command:
         case "search":
             print("Searching for:", args.query)
 
-            with open(movies_path, 'r', encoding='utf-8') as file: 
-               data = json.load(file)
+            with open(movies_path, 'r', encoding='utf-8') as movies_file: 
+               movie_dict = json.load(movies_file)
 
-            matches = search_title(args.query, data)
+            with open(stopwords_path, 'r', encoding='utf-8') as stopwords_file: 
+                stopwords = stopwords_file.read().splitlines()
+
+            punc_table = str.maketrans('', '', string.punctuation)
+            clean_stopwords = []
+            for stopword in stopwords: 
+                clean_word = stopword.translate(punc_table).strip()
+                clean_stopwords.append(clean_word)
+
+            matches = search_title(args.query, movie_dict["movies"], clean_stopwords)
             for index, match in enumerate(matches[:MAX_RESULTS], start=1):
                 print(f"{index}. {match}")
             pass
