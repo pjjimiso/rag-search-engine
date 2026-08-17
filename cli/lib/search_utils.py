7d99746ae8b905e981d74049c9e32c6414ee8277
Patrick Jimison <pat.jimison@gmail.com>
2026-08-17 08:23:05 -0700

    create index and docmap cache files

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -13,10 +13,18 @@ class Movie(TypedDict):
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
+CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
+INDEX_PATH = os.path.join(PROJECT_ROOT, "cache", "index.pkl")
+DOCMAP_PATH = os.path.join(PROJECT_ROOT, "cache", "docmap.pkl")
 
 
 def load_movies() -> list[Movie]:
    with open(DATA_PATH, "r", encoding="utf-8") as f:
       data = json.load(f)
    return data["movies"]
+
+
+def setup_cache() -> None: 
+   if not os.path.exists(CACHE_PATH):
+      os.makedirs(CACHE_PATH)
 
