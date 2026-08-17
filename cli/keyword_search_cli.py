ade8c872c84b805fbc733d805974afeba974396b
Patrick Jimison <pat.jimison@gmail.com>
2026-08-17 08:21:46 -0700

    Add build command for building the inverted index cache

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,6 @@
 import argparse
 
-from lib.keyword_search import search_title
+from lib.keyword_search import search_title, build_command
 
 def main() -> None:
     MAX_RESULTS = 5
@@ -10,6 +10,8 @@ def main() -> None:
 
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
     search_parser.add_argument("query", type=str, help="Search query")
+    
+    build_parser = subparsers.add_parser("build", help="Build index cache")
 
     args = parser.parse_args()
 
@@ -20,6 +22,13 @@ def main() -> None:
             matches = search_title(args.query)
             for index, match in enumerate(matches[:MAX_RESULTS], start=1):
                 print(f"{index}. {match}")
+            pass
+        case "build":
+            print("Building index cache...")
+
+            index = build_command()
+            docs = index.get_documents('merida')
+            print(f"First document for token 'merida' = {docs[0]}")
             pass
         case _:
             parser.print_help()
