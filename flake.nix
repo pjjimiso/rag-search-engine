c3fb1b573876a49cb795a7b26f8307998ce5a18b
Patrick Jimison <pat.jimison@gmail.com>
2026-09-11 10:56:10 -0700

    Add bootdev cli to flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   description = "Python development environment with uv";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.my-config.url = "git+file:///home/pjjimiso/nixos-config"; # bootdev cli
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, my-config }:
     let
       system = "x86_64-linux"; # Adjust for your architecture
       pkgs = import nixpkgs { inherit system; };
       python = pkgs.python312;
+      bootdev = my-config.packages.${system}.bootdev;
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.uv python ];
+        packages = [ pkgs.uv python bootdev ];
 
         # Use the Nix-provided interpreter instead of uv's prebuilt downloads,
         # which are generic dynamically linked binaries NixOS cannot execute.
